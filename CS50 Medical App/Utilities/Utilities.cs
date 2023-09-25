4ff8a57f47d2df5327d67b6921f95683d8e684de
Will Cooke <william.cooke89@gmail.com>
2023-09-25 15:07:49 +0100

    utils

---
+++ CS50 Medical App/Utilities/Utilities.cs
@@ -0,0 +1,23 @@
+﻿//Script for functions for use in other parts of the application
+using CS50_Medical;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Web;
+using System.Globalization;
+
+namespace Medical_Csharp_App.Utilities
+{
+    public class Utility
+    {
+        public static string PatientsConnection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\spark\\source\\repos\\Medical Csharp\\Medical Csharp\\patients.mdf\";Integrated Security=True";
+    }
+}
